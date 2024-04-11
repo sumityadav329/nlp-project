723bad4683e06678f664cb2e2cf19443cbff8111
Sumit Yadav <sumityadav329@gmail.com>
2024-04-11 18:18:44 +0530

    data added

---
+++ demo.py
@@ -0,0 +1,9 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import sys
+
+try: 
+    a = 7/"0"
+
+except Exception as e:
+    raise CustomException(e,sys) from e
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="sumityadav329",
+    author_email="sumityada329@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
