3f1a1d8a341e877035632cf855464c39ea42057a
Sumit Yadav <sumityadav329@gmail.com>
2024-04-13 01:03:26 +0530

    data ingestion added

---
+++ hate/entity/artifact_entity.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+# Data ingestion artifacts
+@dataclass
+class DataIngestionArtifacts:
+    imbalance_data_file_path: str
+    raw_data_file_path: str
+
+
+
+
+@dataclass
+class DataTransformationArtifacts:
+    transformed_data_path: str
